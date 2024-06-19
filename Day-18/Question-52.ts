9ee14bf941da3043412a323a5cccf34a0c12b420
Ahmed1965 <ahmedmuhammed786@gmail.com>
2024-06-19 14:54:45 +0500

    Day-18 Question-52

---
+++ Day-18/Question-52.ts
@@ -0,0 +1,36 @@
+/*
+Q52 - Make a Smartphone Object: Create a simple way to keep track of a smartphone's details. Include its brand, model, and other key features like how much storage it has, the size of its screen, and how long its battery lasts.
+*/
+
+class phones{
+    brand:string;
+    model:string;
+    storage:number;
+    screenSize:number;
+    batteryLife:number;
+
+    constructor(brand:string,model:string,storage:number,screenSize:number,batteryLife:number){
+        this.brand = brand;
+        this.model = model;
+        this.storage = storage;
+        this.screenSize = screenSize;
+        this.batteryLife = batteryLife;
+    }
+    smart_phone(brand:string,model:string,storage:number,screenSize:number,batteryLife:number) {
+        return `The smartphone's brand is ${brand}, model is ${model}, storage capacity is ${storage} GB, screen size is ${screenSize} inches, and battery life is ${batteryLife} hours.`;
+
+    }
+}
+const myPhone = new phones("Samsung", "Galaxy S21", 512, 6.2, 8);
+console.log(myPhone.smart_phone("Samsung", "Galaxy S21", 512, 6.2, 8));
+
+
+
+           
+    
+
+
+
+
+
+
